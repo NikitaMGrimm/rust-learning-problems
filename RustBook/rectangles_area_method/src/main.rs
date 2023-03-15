2f194540d36c04738b7a5e069a56d226b47e967f
Nikita Maximilian Grimm <nikitagrimm03@gmail.com>
2023-03-15 18:47:07 +0100

    explanation

--- RustBook/rectangles_area_method/src/main.rs
+++ RustBook/rectangles_area_method/src/main.rs
@@ -42,6 +42,6 @@ fn main() {
     let square1 = Rectangle::square(100);
     println!("{:?}", square1);
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2)); // Rectangle::can_hold(&rect1, &rect2) is equivalent.
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
