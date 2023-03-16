f980966b67e4102b9c40ab71a63b081e4d312a7c
Nikita Maximilian Grimm <nikitagrimm03@gmail.com>
2023-03-16 17:06:46 +0100

    finished chapter 6

--- RustBook/match_control_flow/src/main.rs
+++ RustBook/match_control_flow/src/main.rs
@@ -1,3 +1,44 @@
+#[derive(Debug)]
+enum Location {
+    Point(i32),
+    Range(i32, i32),
+}
+
+fn print_range_max(loc: &Location) {
+    // print the second field of Range, if loc is a Range
+    if let Location::Range(_, y) = loc {
+        println!("{}", y);
+    }
+}
+
+fn get_start(loc: &Location) -> i32 {
+    match loc {
+        Location::Point(x) => *x,
+        Location::Range(x, _) => *x,
+    }
+    // return the first field of Range or the only field of Point
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {}", max),
+        _ => (),
+    }
+
+    // This can be written like:
+
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+
+    let borrow_test = Location::Range(2, 22);
+    print_range_max(&borrow_test);
+
+    println!("{borrow_test:?}");
+
+    let y = if let Location::Range(_, y) = borrow_test { y }
+    else {0};
+    println!("{}", y);
 }
