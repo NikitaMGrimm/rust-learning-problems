971525b7d8bbc6903b56b767547d8c2853d6849e
Nikita Maximilian Grimm <nikitagrimm03@gmail.com>
2023-03-16 13:40:33 +0100

    match control flow

--- RustBook/enums_and_pattern_matching/src/main.rs
+++ RustBook/enums_and_pattern_matching/src/main.rs
@@ -1,3 +1,14 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+fn route(ip_kind: IpAddrKind) {}
+
 fn main() {
-    println!("Hello, world!");
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
 }
