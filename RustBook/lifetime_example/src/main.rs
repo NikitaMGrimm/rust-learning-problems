147ad84ec40a53e2779de574d52cc158d0fcc7f1
Nikita Maximilian Grimm <nikitagrimm03@gmail.com>
2023-03-18 21:01:15 +0100

    lifetimes

---
+++ RustBook/lifetime_example/src/main.rs
@@ -0,0 +1,22 @@
+use std::fmt::Display;
+
+#[rustfmt::skip]
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
--- RustBook/traits_and_stuff/src/main.rs
+++ RustBook/traits_and_stuff/src/main.rs
@@ -1,3 +1,6 @@
+use std::fmt::Debug;
+use std::fmt::Display;
+
 pub trait Greeting {
     fn greet(&self);
     fn default_greeting(&self) {
@@ -28,6 +31,43 @@ fn hello(hey: &impl Greeting) {
     hey.default_greeting();
 }
 
+fn alternative_hello<T: Greeting>(hey: &T) {
+    println!("\nHELLO!");
+    hey.greet();
+    hey.default_greeting();
+}
+
+// This is ugly:
+fn some_function(t: &(impl Display + Clone), u: &(impl Clone + Debug)) -> i32 {
+    0
+}
+
+// One variant:
+fn alternative0_some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 {
+    0
+}
+
+// Other variant:
+fn alternative1_some_function<T, U>(t: &T, u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+    0
+}
+
+// We can return types that implement a trait!:
+// fn returns_summarizable() -> impl Summary {
+//     Tweet {
+//         username: String::from("horse_ebooks"),
+//         content: String::from(
+//             "of course, as you probably already know, people",
+//         ),
+//         reply: false,
+//         retweet: false,
+//     }
+// }
+
 fn main() {
     let person = Person {
         name: String::from("Alice"),
@@ -36,4 +76,5 @@ fn main() {
     person.default_greeting(); // Output: Welcome, Alice!
 
     hello(&person);
+    alternative_hello(&person);
 }
