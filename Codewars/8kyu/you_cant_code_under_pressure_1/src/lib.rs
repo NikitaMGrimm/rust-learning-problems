53625c1529956ed8eb57bb1766d46508ddc4f632
Nikita Maximilian Grimm <nikitagrimm03@gmail.com>
2023-03-26 22:52:51 +0200

    more easy stuff

---
+++ Codewars/8kyu/you_cant_code_under_pressure_1/src/lib.rs
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/53ee5429ba190077850011d4/train/rust
+
+// Code as fast as you can! You need to double the integer and return it.
+
+fn double_integer(n: i32) -> i32 {
+    n << 1
+}
